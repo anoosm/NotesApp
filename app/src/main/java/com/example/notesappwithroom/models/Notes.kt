ca0bbfc3607086cf71e84033991e1c93e9f095f1
anoosm <anoosalathur@gmail.com>
2022-10-25 12:12:59 +0530

    changes to Notes

--- app/src/main/java/com/example/notesappwithroom/models/Notes.kt
+++ app/src/main/java/com/example/notesappwithroom/models/Notes.kt
@@ -1,3 +1,13 @@
 package com.example.notesappwithroom.models
 
-data class Notes()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notesTable")
+data class Notes(
+    @ColumnInfo(name = "title") val noteTitle: String,
+    @ColumnInfo(name = "description") val desctiption: String,
+    @ColumnInfo(name = "timestamp") val timestamp: String,
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+)
